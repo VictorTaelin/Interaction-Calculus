ae8144350ea7973def41e4527553357e1202e8ef
Victor Maia <victor.taelin@gmail.com>
2023-05-18 18:23:37 -0300

    Organization

---
+++ src/term/mod.rs
@@ -0,0 +1,143 @@
+#![allow(dead_code)]
+
+mod syntax;
+pub use self::syntax::*;
+
+mod convert;
+pub use self::convert::*;
+
+use std::collections::*;
+use inet::*;
+use std;
+
+// Terms of the Interaction Calculus.
+#[derive(Clone, Debug)]
+pub enum Term {
+  // Abstractions
+  Lam {nam: Vec<u8>, bod: Box<Term>},                
+
+  // Applications
+  App {fun: Box<Term>, arg: Box<Term>},
+
+  // Superpositions
+  Sup {tag: u32, fst: Box<Term>, snd: Box<Term>},
+
+  // Duplications
+  Dup {tag: u32, fst: Vec<u8>, snd: Vec<u8>, val: Box<Term>, nxt: Box<Term>},
+
+  // Variables
+  Var {nam: Vec<u8>}, 
+
+  // Erasure
+  Set
+}
+
+use self::Term::{*};
+
+// Source code is Ascii-encoded.
+pub type Str = [u8];
+pub type Chr = u8;
+
+// Converts an index to a name
+pub fn index_to_name(idx : u32) -> Vec<Chr> {
+  let mut name = Vec::new();
+  let mut idx = idx;
+  while idx > 0 {
+    idx = idx - 1;
+    name.push((97 + idx % 26) as u8);
+    idx = idx / 26;
+  }
+  return name;
+}
+
+// Converts a name to an index
+pub fn name_to_index(name : &Vec<Chr>) -> u32 {
+  let mut idx : u32 = 0;
+  for byte in name.iter().rev() {
+    idx = (idx * 26) + (*byte as u32 - 97) + 1;
+  }
+  return idx;
+}
+
+// A context is a vector of (name, value) assignments.
+type Context<'a> = Vec<(&'a Str, Option<Term>)>;
+
+// Extends a context with a (name, value) assignments.
+fn extend<'a,'b>(nam : &'a Str, val : Option<Term>, ctx : &'b mut Context<'a>) -> &'b mut Context<'a> {
+  ctx.push((nam,val));
+  ctx
+}
+
+// Removes an assignment from a context.
+fn narrow<'a,'b>(ctx : &'b mut Context<'a>) -> &'b mut Context<'a> {
+  ctx.pop();
+  ctx
+}
+
+pub fn namespace(space : &Vec<u8>, idx : u32, var : &Vec<u8>) -> Vec<u8> {
+  if var != b"*" {
+    let mut nam = space.clone();
+    nam.extend_from_slice(b"/");
+    nam.append(&mut idx.to_string().as_bytes().to_vec());
+    nam.extend_from_slice(b"/");
+    nam.append(&mut var.clone());
+    nam
+  } else {
+    var.clone()
+  }
+}
+
+// Makes a namespaced copy of a term
+pub fn copy(space : &Vec<u8>, idx : u32, term : &Term) -> Term {
+  match term {
+    Lam{nam, bod} => {
+      let nam = namespace(space, idx, nam);
+      let bod = Box::new(copy(space, idx, bod));
+      Lam{nam, bod}
+    },
+    App{fun, arg} => {
+      let fun = Box::new(copy(space, idx, fun));
+      let arg = Box::new(copy(space, idx, arg));
+      App{fun, arg}
+    },
+    Sup{tag, fst, snd} => {
+      let tag = *tag;
+      let fst = Box::new(copy(space, idx, fst));
+      let snd = Box::new(copy(space, idx, snd));
+      Sup{tag, fst, snd}
+    },
+    Dup{tag, fst, snd, val, nxt} => {
+      let tag = *tag;
+      let fst = namespace(space, idx, fst);
+      let snd = namespace(space, idx, snd);
+      let val = Box::new(copy(space, idx, val));
+      let nxt = Box::new(copy(space, idx, nxt));
+      Dup{tag, fst, snd, val, nxt}
+    },
+    Var{nam} => {
+      let nam = namespace(space, idx, nam);
+      Var{nam}
+    },
+    Set => Set
+  }
+}
+
+// Display macro.
+impl std::fmt::Display for Term {
+  fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+    write!(f, "{}", String::from_utf8_lossy(&to_string(&self)))
+  }
+}
+
+// Reduces an Interaction Calculus term through Interaction Combinators.
+pub fn reduce(term : &Term) -> Term {
+  let mut net : INet = to_net(&term);
+  ::inet::reduce(&mut net);
+  from_net(&net)
+}
+
+pub fn reduce_with_stats(term : &Term) -> (Term, Stats) {
+  let mut net : INet = to_net(&term);
+  let stats = ::inet::reduce(&mut net);
+  (from_net(&net), stats)
+}
