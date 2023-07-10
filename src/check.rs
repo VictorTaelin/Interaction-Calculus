ca257b22d1b2eb94fd7e4f7f653994b486ffbb23
Victor Maia <victor.taelin@gmail.com>
2023-07-10 14:33:58 -0300

    update bin

--- src/check.rs
+++ src/check.rs
@@ -72,154 +72,132 @@ pub fn equal(inet: &mut INet, a: Port, b: Port) -> bool {
   let mut b_logs = vec![];
   let mut a_cursor = Cursor { root: a, prev: a, path: &mut a_path, logs: &mut a_logs, };
   let mut b_cursor = Cursor { root: b, prev: b, path: &mut b_path, logs: &mut b_logs, };
-  compare(inet, &mut a_cursor, &mut b_cursor)
+  compare(inet, &mut a_cursor, &mut b_cursor, true)
 }
 
 // Compares two cursors by moving them forward until root is reached
-pub fn compare(inet: &mut INet, a: &mut Cursor, b: &mut Cursor) -> bool {
+pub fn compare(inet: &mut INet, a: &mut Cursor, b: &mut Cursor, flip: bool) -> bool {
   //println!("Equal: (Node: {}, Slot: {}) ~ (Node {}, Slot {})\n  Paths: {:?} | {:?}\n  Logs : {:?} | {:?}", addr(a.prev), slot(a.prev), addr(b.prev), slot(b.prev), a.path, b.path, a.logs, b.logs);
   
-  // Moves one of the cursors forward and compares
-  fn advance(inet: &mut INet, a: &mut Cursor, b: &mut Cursor, flip: bool) -> Option<bool> {
+  let a_next = enter(inet, a.prev);
+  let a_slot = slot(a_next);
+  let a_kind = kind(inet, addr(a_next));
 
-    //reduce(inet, a.prev, &|ak, bk| ak == FIX || bk == FIX);
+  // If on root, stop
+  if a_next == a.root || a_next == ROOT || a_kind == ERA {
+    if flip {
+      return compare(inet, b, a, false);
+    } else {
+      return a.path.get(&ANN) != b.path.get(&ANN) || a.path.get(&CON) == b.path.get(&CON);
+    }
 
-    let a_next = enter(inet, a.prev);
-    let a_slot = slot(a_next);
-    let a_kind = kind(inet, addr(a_next));
-
-    // If on root, stop
-    if a_next == a.root || a_next == ROOT {
-      return None;
-
-    // If on a fixed point, stop
-    } else if a_kind == FIX {
-      return None;
-
-    } else if a_kind == ERA {
-      return None;
-
-    // If entering main port...
+  // If entering main port...
   } else if a_slot == 0 {
 
-      // If deque isn't empty, pop_back a slot and move to it
-      if let Some(slot) = a.pop_back(a_kind) {
-        a.logs.push(format!("V{}", slot));
-        let an = &mut a.next(inet, slot);
-        let eq = if flip { compare(inet, b, an) } else { compare(inet, an, b) };
-        a.logs.pop();
-        a.push_back(a_kind, slot);
-        return Some(eq);
-
-      // If deque is empty, move to slots [1,2] and push_front to the *other* deque
-      } else {
-        //println!("enter main (split)");
-        for slot in [2,1] {
-          a.logs.push(format!("W{}", slot));
-          b.push_front(a_kind, slot);
-          let an = &mut a.next(inet, slot);
-          let eq = if flip { compare(inet, b, an) } else { compare(inet, an, b) };
-          a.logs.pop();
-          b.pop_front(a_kind);
-          if !eq {
-            return Some(false);
-          }
-        }
-        return Some(true);
-      }
-
-    // If entering an aux port, push_back that slot to the deque, and move to the main port
-    } else {
-      a.logs.push(format!("^{}", a_slot));
-      a.push_back(a_kind, slot(enter(inet, a.prev)) as u8);
-      let an = &mut a.next(inet, 0);
-      let eq = if flip { compare(inet, b, an) } else { compare(inet, an, b) };
-      a.pop_back(a_kind);
+    // If deque isn't empty, pop_back a slot and move to it
+    if let Some(slot) = a.pop_back(a_kind) {
+      a.logs.push(format!("V{}", slot));
+      let an = &mut a.next(inet, slot);
+      let eq = compare(inet, an, b, flip);
       a.logs.pop();
-      return Some(eq);
+      a.push_back(a_kind, slot);
+      return eq;
+
+    // If deque is empty, move to slots [1,2] and push_front to the *other* deque
+    } else {
+      //println!("enter main (split)");
+      for slot in [2,1] {
+        a.logs.push(format!("W{}", slot));
+        b.push_front(a_kind, slot);
+        let an = &mut a.next(inet, slot);
+        let eq = compare(inet, an, b, flip);
+        a.logs.pop();
+        b.pop_front(a_kind);
+        if !eq {
+          return false;
+        }
+      }
+      return true;
     }
-  }
 
-  // If 'a' can be advanced, advance it and compare
-  if let Some(eq) = advance(inet, a, b, false) {
+  // If entering an aux port, push_back that slot to the deque, and move to the main port
+  } else {
+    a.logs.push(format!("^{}", a_slot));
+    a.push_back(a_kind, slot(enter(inet, a.prev)) as u8);
+    let an = &mut a.next(inet, 0);
+    let eq = compare(inet, an, b, flip);
+    a.pop_back(a_kind);
+    a.logs.pop();
     return eq;
   }
+}
 
-  // If 'b' can be advanced, advance it and compare
-  if let Some(eq) = advance(inet, b, a, true) {
-    return eq;
-  }
+// FIX-POINT COMPARISON (removed for now)
+// --------------------------------------
 
-  // If both are fixed-point, check `@x (f (g x)) == @x (g (f x))`
-  
-  //if false { // TODO: re-add once the TODO's are done
-    //let a_next = enter(inet, a.prev);
-    //let b_next = enter(inet, b.prev);
-    //if kind(inet,addr(a_next)) == FIX && kind(inet,addr(b_next)) == FIX {
-      //// If both ports are different, apply the fixpose transformation and compare
-      //if a_next != b_next {
-        //fixpose(inet, a_next, b_next);
-        //return compare(inet, a, b);
-      //}
-      //// If both ports are identical on slot 2, enter the merged fixpoint and compare
-      //if slot(a_next) == 2 {
-        //let a = &mut a.next(inet,0);
-        //let b = &mut b.next(inet,0);
-        //return compare(inet, a, b);
-      //}
-    //}
+//// If both are fixed-point, check `@x (f (g x)) == @x (g (f x))`
+////if false { // TODO: re-add once the TODO's are done
+  ////let a_next = enter(inet, a.prev);
+  ////let b_next = enter(inet, b.prev);
+  ////if kind(inet,addr(a_next)) == FIX && kind(inet,addr(b_next)) == FIX {
+    ////// If both ports are different, apply the fixpose transformation and compare
+    ////if a_next != b_next {
+      ////fixpose(inet, a_next, b_next);
+      ////return compare(inet, a, b);
+    ////}
+    ////// If both ports are identical on slot 2, enter the merged fixpoint and compare
+    ////if slot(a_next) == 2 {
+      ////let a = &mut a.next(inet,0);
+      ////let b = &mut b.next(inet,0);
+      ////return compare(inet, a, b);
+    ////}
+  ////}
 
-    //if kind(inet,addr(a_next)) == FIX && kind(inet,addr(b_next)) != FIX {
-      //todo!()
-    //}
+  ////if kind(inet,addr(a_next)) == FIX && kind(inet,addr(b_next)) != FIX {
+    ////todo!()
+  ////}
 
-    //if kind(inet,addr(a_next)) != FIX && kind(inet,addr(b_next)) == FIX {
-      //todo!()
-    //}
+  ////if kind(inet,addr(a_next)) != FIX && kind(inet,addr(b_next)) == FIX {
+    ////todo!()
+  ////}
+////}
+
+//// Composes two fixed points:
+//// @x(F x)     , @y(G y)
+//// -------------------------- fixpose
+//// @x(F (G x)) , @x(G (F x)))
+//// https://twitter.com/VictorTaelin/status/1659724812057452549
+//fn fixpose(inet: &mut INet, a: Port, b: Port) {
+  //if kind(inet,addr(a)) == FIX
+  //&& kind(inet,addr(b)) == FIX
+  //&& slot(a) == 2
+  //&& slot(b) == 2 {
+    //let ff = enter(inet, port(addr(a), 0));
+    //let fx = enter(inet, port(addr(a), 1));
+    //let gg = enter(inet, port(addr(b), 0));
+    //let gx = enter(inet, port(addr(b), 1));
+    //let fu = new_node(inet, FIX + 1);
+    //let fd = new_node(inet, FIX + 1);
+    //let gu = new_node(inet, FIX + 1);
+    //let gd = new_node(inet, FIX + 1);
+    //let up = new_node(inet, FIX + 1);
+    //let dw = new_node(inet, FIX + 1);
+    //let cc = new_node(inet, FIX);
+    //let rt = new_node(inet, FIX + 1);
+    //link(inet, port(fu,0), ff);
+    //link(inet, port(fu,1), port(gd,2));
+    //link(inet, port(fu,2), port(up,1));
+    //link(inet, port(gu,0), gg);
+    //link(inet, port(gu,1), port(fd,2));
+    //link(inet, port(gu,2), port(up,2));
+    //link(inet, port(fd,0), fx);
+    //link(inet, port(fd,1), port(dw,2));
+    //link(inet, port(gd,0), gx);
+    //link(inet, port(gd,1), port(dw,1));
+    //link(inet, port(cc,0), port(up,0));
+    //link(inet, port(cc,1), port(dw,0));
+    //link(inet, port(cc,2), port(rt,0));
+    //link(inet, port(rt,1), enter(inet,a));
+    //link(inet, port(rt,2), enter(inet,b));
   //}
-
-  //println!("== {:?} {:?} {}", a.path, b.path, a.path.get(&ANN) != b.path.get(&ANN) || a.path.get(&CON) == b.path.get(&CON));
-  
-  // If we've reached a final port (root or fix), compare the unconsumed paths 
-  return a.path.get(&ANN) != b.path.get(&ANN) || a.path.get(&CON) == b.path.get(&CON);
-}
-
-// Composes two fixed points:
-// @x(F x)     , @y(G y)
-// -------------------------- fixpose
-// @x(F (G x)) , @x(G (F x)))
-// https://twitter.com/VictorTaelin/status/1659724812057452549
-fn fixpose(inet: &mut INet, a: Port, b: Port) {
-  if kind(inet,addr(a)) == FIX
-  && kind(inet,addr(b)) == FIX
-  && slot(a) == 2
-  && slot(b) == 2 {
-    let ff = enter(inet, port(addr(a), 0));
-    let fx = enter(inet, port(addr(a), 1));
-    let gg = enter(inet, port(addr(b), 0));
-    let gx = enter(inet, port(addr(b), 1));
-    let fu = new_node(inet, FIX + 1);
-    let fd = new_node(inet, FIX + 1);
-    let gu = new_node(inet, FIX + 1);
-    let gd = new_node(inet, FIX + 1);
-    let up = new_node(inet, FIX + 1);
-    let dw = new_node(inet, FIX + 1);
-    let cc = new_node(inet, FIX);
-    let rt = new_node(inet, FIX + 1);
-    link(inet, port(fu,0), ff);
-    link(inet, port(fu,1), port(gd,2));
-    link(inet, port(fu,2), port(up,1));
-    link(inet, port(gu,0), gg);
-    link(inet, port(gu,1), port(fd,2));
-    link(inet, port(gu,2), port(up,2));
-    link(inet, port(fd,0), fx);
-    link(inet, port(fd,1), port(dw,2));
-    link(inet, port(gd,0), gx);
-    link(inet, port(gd,1), port(dw,1));
-    link(inet, port(cc,0), port(up,0));
-    link(inet, port(cc,1), port(dw,0));
-    link(inet, port(cc,2), port(rt,0));
-    link(inet, port(rt,1), enter(inet,a));
-    link(inet, port(rt,2), enter(inet,b));
-  }
-}
+//}
--- src/main.rs
+++ src/main.rs
@@ -27,21 +27,39 @@ fn main() {
   let matches = App::new("My App")
     .version("1.0")
     .author("Victor Taelin <victor.taelin@gmail.com>")
-    .about("Interaction Calculus CLI")
+    .about("Interaction Type Theory CLI")
     .arg(Arg::with_name("file")
       .help("The input file to process")
       .required(true)
       .index(1))
     .get_matches();
 
+  // Reads source file to a string
   let file_name = matches.value_of("file").unwrap();
   let mut file = File::open(file_name).expect("Unable to open the file");
   let mut code = String::new();
   file.read_to_string(&mut code).expect("Unable to read the file");
+
+  // Converts source to term
+  let term = from_string(code.as_bytes());
+
+  // Converts term to inet
+  let mut inet = new_inet();
+  inject(&mut inet, &term, ROOT);
+
+  // Normalizes
+  normal(&mut inet, ROOT);
+
+  println!("{}", show(&inet, ROOT));
+  println!("\x1b[90m{:?} rewrites\x1b[0m", inet.rules);
+
+  println!("Check? \n{}", if check(&mut inet, ROOT) { "✓" } else { "✗" });
+  println!("");
 
   let term = term::from_string(code.as_bytes());
   let (norm, rules) = term::normalize(&term);
 
   println!("{}\n", norm);
   println!("{:?} rewrites", rules);
+
 }
