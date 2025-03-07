53adb32d337a65308d6f77ed5c275d2ed278e56f
Victor Taelin <victor.taelin@gmail.com>
2025-03-07 18:29:12 -0300

    ic_normal() has a bug

--- src/ic.c
+++ src/ic.c
@@ -1,3 +1,7 @@
+//./collapse.h//
+//./collapse.c//
+//./ic.h//
+
 /**
  * Interaction Calculus (IC) - Implementation file
  * 
@@ -8,52 +12,72 @@
 
 #include "ic.h"
 
-// Reduce a term to full normal form by recursively applying WHNF.
-// 
-// @param ic The IC context
-// @param term The term to normalize
-// @return The fully normalized term
+// FIXME: isn't properly reducing: 
+// !not = &0{λb0.λt0.λf0.((b0 t0) f0), λb1.λt1.λf1.((b1 t1) f1)};
+// (not λt.λf.t)
+// why?
+
+//// Reduce a term to full normal form by recursively applying WHNF.
+//// 
+//// @param ic The IC context
+//// @param term The term to normalize
+//// @return The fully normalized term
+//Term ic_normal(IC* ic, Term term) {
+  //// Reset stack
+  //ic->stack_pos = 0;
+  //Term* heap = ic->heap;
+  //Term* stack = ic->stack;
+  //uint32_t stack_pos = 0;
+
+  //// Allocate a new node for the initial term
+  //uint32_t root_loc = ic_alloc(ic, 1);
+  //heap[root_loc] = term;
+
+  //// Push initial location to stack as a "location"
+  //stack[stack_pos++] = ic_make_term(VAR, root_loc);
+
+  //while (stack_pos > 0) {
+    //// Pop current location from stack
+    //uint32_t loc = TERM_VAL(stack[--stack_pos]);
+
+    //// Takes whnf
+    //heap[loc] = ic_whnf(ic, heap[loc]);
+
+    //// Get term details
+    //TermTag tag = TERM_TAG(heap[loc]);
+    //uint32_t val = TERM_VAL(heap[loc]);
+
+    //// Push subterm locations based on term type
+    //if (tag == LAM) {
+      //stack[stack_pos++] = ic_make_term(VAR, val + 0);
+    //} else if (tag == APP || IS_SUP(tag)) {
+      //stack[stack_pos++] = ic_make_term(VAR, val + 0);
+      //stack[stack_pos++] = ic_make_term(VAR, val + 1);
+    //}
+  //}
+
+  //// Update stack position and return the fully normalized term
+  //ic->stack_pos = stack_pos;
+  //return heap[root_loc];
+//}
+
+// Recursive implementation of normal form reduction
 Term ic_normal(IC* ic, Term term) {
-  // Reset stack
-  ic->stack_pos = 0;
-  Term* heap = ic->heap;
-  Term* stack = ic->stack;
-  uint32_t stack_pos = 0;
-
-  // Allocate a new node for the initial term
-  uint32_t root_loc = ic_alloc(ic, 1);
-  heap[root_loc] = term;
-
-  // Push initial location to stack as a "location"
-  stack[stack_pos++] = ic_make_term(VAR, root_loc);
-
-  while (stack_pos > 0) {
-    // Pop current location from stack
-    uint32_t loc = TERM_VAL(stack[--stack_pos]);
-
-    // Get term at this location
-    Term current = heap[loc];
-
-    // Reduce to WHNF
-    current = ic_whnf(ic, current);
-
-    // Store the WHNF term back to the heap
-    heap[loc] = current;
-
-    // Get term details
-    TermTag tag = TERM_TAG(current);
-    uint32_t val = TERM_VAL(current);
-
-    // Push subterm locations based on term type
-    if (tag == LAM) {
-      stack[stack_pos++] = ic_make_term(VAR, val + 0);
-    } else if (tag == APP || IS_SUP(tag)) {
-      stack[stack_pos++] = ic_make_term(VAR, val + 0);
-      stack[stack_pos++] = ic_make_term(VAR, val + 1);
-    }
+  term = ic_whnf(ic, term);
+  TermTag tag = TERM_TAG(term);
+  uint32_t loc = TERM_VAL(term);
+  if (tag == LAM) {
+    ic->heap[loc] = ic_normal(ic, ic->heap[loc]);
+    return term;
+  } else if (tag == APP) {
+    ic->heap[loc+0] = ic_normal(ic, ic->heap[loc]);
+    ic->heap[loc+1] = ic_normal(ic, ic->heap[loc+1]);
+    return term;
+  } else if (IS_SUP(tag)) {
+    ic->heap[loc+0] = ic_normal(ic, ic->heap[loc]);
+    ic->heap[loc+1] = ic_normal(ic, ic->heap[loc+1]);
+    return term;
+  } else {
+    return term;
   }
-
-  // Update stack position and return the fully normalized term
-  ic->stack_pos = stack_pos;
-  return heap[root_loc];
 }
